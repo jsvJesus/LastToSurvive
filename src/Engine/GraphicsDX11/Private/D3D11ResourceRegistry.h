ee8094a6838ecc31c74258d7584214fc713ecd1d
Jesus <52017003+jsvJesus@users.noreply.github.com>
2026-07-14 01:12:12 +0300

    graphics: extend DX11 resource registry for shader pipelines

--- src/Engine/GraphicsDX11/Private/D3D11ResourceRegistry.h
+++ src/Engine/GraphicsDX11/Private/D3D11ResourceRegistry.h
@@ -3,7 +3,10 @@
 #include "D3D11HandlePool.h"
 #include "D3D11ResourceTypes.h"
 #include "Graphics/GraphicsResult.h"
+#include "Graphics/InputLayout.h"
+#include "Graphics/PipelineState.h"
 #include "Graphics/ResourceHandle.h"
+#include "Graphics/Shader.h"
 
 #include <cstddef>
 
@@ -33,18 +36,61 @@ namespace engine::graphics::d3d11::detail
         [[nodiscard]] GraphicsResult DestroyBuffer(
             BufferHandle buffer) noexcept;
 
+        [[nodiscard]] GraphicsResult CreateShader(
+            ID3D11Device* device,
+            const ShaderDesc& desc,
+            ShaderHandle& outShader) noexcept;
+
+        [[nodiscard]] GraphicsResult DestroyShader(
+            ShaderHandle shader) noexcept;
+
+        [[nodiscard]] GraphicsResult CreateInputLayout(
+            ID3D11Device* device,
+            const InputLayoutDesc& desc,
+            InputLayoutHandle& outInputLayout) noexcept;
+
+        [[nodiscard]] GraphicsResult DestroyInputLayout(
+            InputLayoutHandle inputLayout) noexcept;
+
+        [[nodiscard]] GraphicsResult CreateGraphicsPipeline(
+            ID3D11Device* device,
+            const GraphicsPipelineDesc& desc,
+            PipelineStateHandle& outPipeline) noexcept;
+
+        [[nodiscard]] GraphicsResult DestroyGraphicsPipeline(
+            PipelineStateHandle pipeline) noexcept;
+
         void Clear() noexcept;
 
         [[nodiscard]] const D3D11TextureResource* GetTexture(
             TextureHandle texture) const noexcept;
+
         [[nodiscard]] const D3D11BufferResource* GetBuffer(
             BufferHandle buffer) const noexcept;
 
+        [[nodiscard]] const D3D11ShaderResource* GetShader(
+            ShaderHandle shader) const noexcept;
+
+        [[nodiscard]] const D3D11InputLayoutResource* GetInputLayout(
+            InputLayoutHandle inputLayout) const noexcept;
+
+        [[nodiscard]] const D3D11GraphicsPipelineResource*
+            GetGraphicsPipeline(
+                PipelineStateHandle pipeline) const noexcept;
+
         [[nodiscard]] std::size_t GetTextureCount() const noexcept;
         [[nodiscard]] std::size_t GetBufferCount() const noexcept;
+        [[nodiscard]] std::size_t GetShaderCount() const noexcept;
+        [[nodiscard]] std::size_t GetInputLayoutCount() const noexcept;
+        [[nodiscard]] std::size_t GetGraphicsPipelineCount() const noexcept;
 
     private:
         HandlePool<TextureHandle, D3D11TextureResource> textures_;
         HandlePool<BufferHandle, D3D11BufferResource> buffers_;
+        HandlePool<ShaderHandle, D3D11ShaderResource> shaders_;
+        HandlePool<InputLayoutHandle, D3D11InputLayoutResource>
+            inputLayouts_;
+        HandlePool<PipelineStateHandle, D3D11GraphicsPipelineResource>
+            graphicsPipelines_;
     };
 }
