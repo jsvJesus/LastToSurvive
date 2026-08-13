40262d1224b564b52962e584ed99127f1f02ff4d
Jesus <es3n1n.eng@gmail.com>
2026-08-14 00:21:39 +0300

    fix Terrain Assets.

--- src/Engine/Assets/Private/TerrainAsset.cpp
+++ src/Engine/Assets/Private/TerrainAsset.cpp
@@ -4,14 +4,10 @@
 #include <cmath>
 #include <cstdint>
 #include <fstream>
-#include <limits>
 #include <new>
 #include <utility>
 
-#if defined(_WIN32)
-#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-#endif
 
 namespace engine::assets
 {
